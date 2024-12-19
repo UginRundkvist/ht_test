8fde7ee4d28eeceaf10e5ccdf77817d40f2c2a54
Ugin <alrund@gmail.com>
2024-12-19 19:11:42 +0000

    0

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,32 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	result := ""
+	length := len(input)
+
+	for i := 0; i < length; i++ {
+		current := input[i]
+
+		if unicode.IsDigit(rune(current)) {
+			if i == 0 || unicode.IsDigit(rune(input[i-1])) {
+				return "", ErrInvalidString
+			}
+
+			count := int(current)
+			if count > 0 {
+				for j := 0; j < count-1; j++ {
+					result += string(input[i-1])
+				}
+			}
+		} else {
+			result += string(current)
+		}
+	}
+	return result, nil
 }
