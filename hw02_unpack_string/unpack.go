bcf611b1ac2a4337e08c14fdefdfba910d5bca68
Ugin <alrund@gmail.com>
2024-12-21 19:36:36 +0000

    00000

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,27 +2,38 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"unicode"
+	"strconv"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
+func isSingleCharNumber(str string) bool {
+	_, err := strconv.Atoi(str)
+	return err == nil
+}
+
 func Unpack(input string) (string, error) {
 	result := ""
-	length := len(input)
+	runes := []rune(input)
+	var symbols []string
+
+	for _, r := range runes {
+		symbols = append(symbols, string(r))
+	}
+	length := len(symbols)
 
 	for i := 0; i < length; i++ {
-		current := input[i]
+		current := symbols[i]
 
-		if unicode.IsDigit(rune(current)) {
-			if i == 0 || unicode.IsDigit(rune(input[i-1])) {
+		if isSingleCharNumber(current) {
+			if i == 0 || isSingleCharNumber(symbols[i-1]) {
 				return "", ErrInvalidString
 			}
 
-			count := int(current)
+			count, _ := strconv.Atoi(current)
 			if count > 0 {
 				for j := 0; j < count-1; j++ {
-					result += string(input[i-1])
+					result += symbols[i-1]
 				}
 			}
 		} else {
