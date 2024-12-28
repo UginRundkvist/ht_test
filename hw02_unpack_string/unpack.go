658f90f5d6c688f1940982beded91b5fb58bc715
Ugin <alrund@gmail.com>
2024-12-28 17:45:41 +0000

    12

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -3,20 +3,22 @@ package hw02unpackstring
 import (
 	"errors"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(input string) (string, error) {
-	var result string
+	var result strings.Builder
 	runes := []rune(input)
 
 	for i := 0; i < len(runes); i++ {
 		current := runes[i]
 
 		if !unicode.IsDigit(current) {
-			result += string(current)
+			// result += string(current)
+			result.WriteString(string(current))
 		} else {
 			if i == 0 || unicode.IsDigit(rune(input[i-1])) {
 				return "", ErrInvalidString
@@ -25,12 +27,14 @@ func Unpack(input string) (string, error) {
 			count, _ := strconv.Atoi(string(current))
 			if count > 0 {
 				for j := 0; j < count-1; j++ {
-					result += string(input[i-1])
+					result.WriteString(string(input[i-1]))
 				}
 			} else if count == 0 {
-				result = result[:len(result)-1]
+				s := result.String()
+				result.Reset()
+				result.WriteString(s[:len(s)-1])
 			}
 		}
 	}
-	return result, nil
+	return result.String(), nil
 }
