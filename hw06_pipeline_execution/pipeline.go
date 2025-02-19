34d7b759b2864756f4432b46ed8aa78533f7feb2
Ugin <alrund@gmail.com>
2025-02-19 19:56:58 +0000

    6

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,10 +1,5 @@
 package hw06pipelineexecution
 
-import (
-	"fmt"
-	"time"
-)
-
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -14,39 +9,19 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	start := time.Now()
-	time.Sleep(1000)
-	new1 := make(In)
-	new2 := make(In)
-	new3 := make(In)
-	select {
-	case <-done:
-		return make(In)
-	default:
-		new1 = stages[0](in)
+	for _, stage := range stages {
+		in = execStage(in, done, stage)
 	}
+	return in
+}
 
-	select {
-	case <-done:
-		return make(In)
-	default:
-		new2 = stages[1](new1)
-	}
-
-	select {
-	case <-done:
-		return make(In)
-	default:
-		new3 = stages[2](new2)
-	}
-
-	select {
-	case <-done:
-		return make(In)
-	default:
-		elapsed := time.Since(start)
-		fmt.Println("Время выполнения: ", elapsed)
-		return (stages[3](new3))
-	}
-
+func execStage(in In, done In, stage Stage) In {
+	go func() {
+		select {
+		case <-done:
+		default:
+			in = stage(in)
+		}
+	}()
+	return in
 }
