593824762b625f3ca05c08a3da78ac290c91b70e
Ugin <alrund@gmail.com>
2025-03-29 14:50:32 +0000

    без линтеров

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -3,6 +3,9 @@ package hw09structvalidator
 import (
 	"fmt"
 	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type ValidationError struct {
@@ -13,38 +16,171 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var errMsgs []string
+	for _, err := range v {
+		errMsgs = append(errMsgs, fmt.Sprintf("%s: %s", err.Field, err.Err))
+	}
+	return strings.Join(errMsgs, ", ")
 }
 
 func Validate(v interface{}) error {
+	var errors ValidationErrors
 	val := reflect.ValueOf(v)
+	if val.Kind() != reflect.Struct && val.Kind() != reflect.Ptr {
+		return fmt.Errorf("ожиалась структура или указатель %s", val.Kind())
+	}
+
+	if val.Kind() == reflect.Ptr && val.IsNil() {
+		return nil
+	}
+
 	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
 
-	if val.Kind() != reflect.Struct {
-		return fmt.Errorf("ожидалась структура, получено: %s", val.Kind())
-	}
+	t := val.Type()
 
-	var errors ValidationErrors
-
-	for i := 0; i < val.Type().NumField(); i++ {
-		field := val.Type().Field(i)
-		fieldValue := val.Field(i)
-
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
 		validateTag := field.Tag.Get("validate")
-
 		if validateTag == "" {
 			continue
 		}
 
-		fmt.Printf("Fild: %s, Tag: %s, Value: %v\n", field.Name, validateTag, fieldValue.Interface())
+		fieldValue := val.Field(i)
+		fieldName := field.Name
 
+		rules := strings.Split(validateTag, "|")
+		for _, rule := range rules {
+			parts := strings.SplitN(rule, ":", 2)
+			validatorName := parts[0]
+			var validatorArgs string
+			if len(parts) > 1 {
+				validatorArgs = parts[1]
+			}
+
+			switch fieldValue.Kind() {
+			case reflect.Int:
+				num := fieldValue.Int()
+				if err := validateInt(num, validatorName, validatorArgs); err != nil {
+					errors = append(errors, ValidationError{Field: fieldName, Err: err})
+				}
+			case reflect.String:
+				str := fieldValue.String()
+				if err := validateString(str, validatorName, validatorArgs); err != nil {
+					errors = append(errors, ValidationError{Field: fieldName, Err: err})
+				}
+			case reflect.Slice:
+				err := validateSlice(fieldValue, fieldName, validatorArgs, errors, validatorName)
+				if err != nil {
+					errors = append(errors, ValidationError{Field: fieldName, Err: err})
+				}
+			default:
+				fmt.Printf("Неверный тип поля: %s\n", fieldValue.Kind())
+			}
+		}
 	}
 
 	if len(errors) > 0 {
 		return errors
 	}
 
+	return nil
+}
+
+func validateSlice(fieldValue reflect.Value, fieldName string, validatorArgs string, errors ValidationErrors, validatorName string) error {
+	for j := 0; j < fieldValue.Len(); j++ {
+		elementValue := fieldValue.Index(j)
+		switch elementValue.Kind() {
+		case reflect.Int:
+			num := elementValue.Int()
+			if err := validateInt(num, validatorName, validatorArgs); err != nil {
+				errors = append(errors, ValidationError{Field: fieldName, Err: err})
+			}
+		case reflect.String:
+			str := elementValue.String()
+			if err := validateString(str, validatorName, validatorArgs); err != nil {
+				errors = append(errors, ValidationError{Field: fieldName, Err: err})
+			}
+		default:
+			fmt.Printf("Неверный элемент слайса: %s\n", elementValue.Kind())
+		}
+	}
+	return nil
+}
+
+func validateInt(num int64, validatorName, validatorArgs string) error {
+	switch validatorName {
+	case "min":
+		minValue, err := strconv.ParseInt(validatorArgs, 10, 64)
+		if err != nil {
+			return fmt.Errorf("неверное минимальное значение %w", err)
+		}
+		if num < minValue {
+			return fmt.Errorf("должно быть больше %d", minValue)
+		}
+	case "max":
+		maxValue, err := strconv.ParseInt(validatorArgs, 10, 64)
+		if err != nil {
+			return fmt.Errorf("неверное максимальное значение: %w", err)
+		}
+		if num > maxValue {
+			return fmt.Errorf("должно быть меньше %d", maxValue)
+		}
+	case "in":
+		allowedValues := strings.Split(validatorArgs, ",")
+		found := false
+		for _, val := range allowedValues {
+			intValue, err := strconv.ParseInt(val, 10, 64)
+			if err != nil {
+				return fmt.Errorf("неверное значение для 'in' %w", err)
+			}
+			if num == intValue {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return fmt.Errorf("должно быть в: %s", validatorArgs)
+		}
+	default:
+		return fmt.Errorf("неизвесный валидатор %s", validatorName)
+	}
+	return nil
+}
+
+func validateString(str string, validatorName, validatorArgs string) error {
+	switch validatorName {
+	case "len":
+		expectedLen, err := strconv.Atoi(validatorArgs)
+		if err != nil {
+			return fmt.Errorf("неверное строковое значение: %w", err)
+		}
+		if len(str) != expectedLen {
+			return fmt.Errorf("длинна должна быть %d", expectedLen)
+		}
+	case "regexp":
+		re, err := regexp.Compile(validatorArgs)
+		if err != nil {
+			return fmt.Errorf("неверный regexp: %w", err)
+		}
+		if !re.MatchString(str) {
+			return fmt.Errorf("должно соответствовать регулярному выражению '%s'", validatorArgs)
+		}
+	case "in":
+		allowedValues := strings.Split(validatorArgs, ",")
+		found := false
+		for _, val := range allowedValues {
+			if str == val {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return fmt.Errorf("должно быть одно из: %s", validatorArgs)
+		}
+	default:
+		return fmt.Errorf("неизвесный валидатор: %s", validatorName)
+	}
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -8,7 +8,6 @@ import (
 
 type UserRole string
 
-// Test the function on different structures and other types.
 type (
 	User struct {
 		ID     string `json:"id" validate:"len:36"`
@@ -24,11 +23,7 @@ type (
 		Version string `validate:"len:5"`
 	}
 
-	Token struct {
-		Header    []byte
-		Payload   []byte
-		Signature []byte
-	}
+	// }.
 
 	Response struct {
 		Code int    `validate:"in:200,404,500"`
@@ -37,38 +32,71 @@ type (
 )
 
 func TestValidate(t *testing.T) {
-	// tests := []struct {
-	// 	in          interface{}
-	// 	expectedErr error
-	// }{
-	// 	{
-	// 		// Place your code here.
-	// 	},
-	// 	// ...
-	// 	// Place your code here.
-	// }
+	tests := []struct {
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			in: User{
+				ID:     "a1b2c3d4-e5f6-7890-1234-567890abcdef",
+				Age:    25,
+				Email:  "test@example.com",
+				Role:   "admin",
+				Phones: []string{"12345678901", "98765432109"},
+			},
+			expectedErr: nil,
+		},
+		{
+			in: App{
+				Version: "0.0.1.0",
+			},
+			expectedErr: ValidationErrors{{Field: "Version", Err: fmt.Errorf("длинна должна быть %d", 5)}},
+		},
+		{
+			in: Response{
+				Code: 200,
+				Body: "OK",
+			},
+			expectedErr: nil,
+		},
+	}
 
-	// for i, tt := range tests {
-	// 	t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
-	// 		tt := tt
-	// 		t.Parallel()
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			tt := tt
+			t.Parallel()
 
-	// 		// Place your code here.
-	// 		_ = tt
-	// 	})
-	// }
-	t.Run("My case", func(t *testing.T) {
-		type Example struct {
-			Name string `validate:"len:32"`
-			Age  int    `validate:"min:10,max:20"`
-			City string
-		}
-		example := Example{Name: "Test", Age: 25, City: "Moscow"}
-		err := Validate(example)
-		if err != nil {
-			fmt.Println("Ошибка валидации:", err)
-		} else {
-			fmt.Println("Валидация прошла успешно")
-		}
-	})
+			err := Validate(tt.in)
+
+			if (err != nil) != (tt.expectedErr != nil) {
+				t.Errorf("ошибка валидации %v, ожидаемая ошибка %v", err, tt.expectedErr)
+				return
+			}
+
+			if tt.expectedErr != nil {
+				expectedVErr, ok := tt.expectedErr.(ValidationErrors)
+				if ok {
+					actualVErr, ok2 := err.(ValidationErrors)
+					if !ok2 {
+						t.Errorf("ожидается массив ошибок, но получено: %T", err)
+						return
+					}
+
+					if len(actualVErr) != len(expectedVErr) {
+						t.Errorf("Ожидаемое колличество ошибок %d, получено %d", len(expectedVErr), len(actualVErr))
+						return
+					}
+
+					for i := range expectedVErr {
+						if actualVErr[i].Err.Error() != expectedVErr[i].Err.Error() || actualVErr[i].Field != expectedVErr[i].Field {
+							t.Errorf("ожидается : %v, получено: %v", expectedVErr[i], actualVErr[i])
+						}
+					}
+				}
+				if err != tt.expectedErr {
+					t.Errorf("ошибка валидации = %v, ожидаемая ошибка %v", err, tt.expectedErr)
+				}
+			}
+		})
+	}
 }
