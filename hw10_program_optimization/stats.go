2b4f00a1ca4fa7385354410047be460f96120a68
Ugin <alrund@gmail.com>
2025-04-03 15:54:35 +0000

    260_57

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,6 +1,7 @@
 package hw10programoptimization
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"strings"
@@ -16,37 +17,50 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	return countDomains(u, domain)
 }
 
-func getUsers(r io.Reader) ([]string, error) {
-	content, err := io.ReadAll(r) // изменить
-	//line, err := r.ReadString('\n')
-	if err != nil {
-		return nil, err
-	}
-	lines := strings.Split(string(content), "\n")
-	return lines, nil
+func getUsers(r io.Reader) (*bufio.Reader, error) {
+	bufferedReader := bufio.NewReader(r)
+	return bufferedReader, nil
 }
 
-func countDomains(u []string, domain string) (DomainStat, error) {
+func countDomains(u *bufio.Reader, domain string) (DomainStat, error) {
 	stat := make(DomainStat)
-	for _, line := range u {
-		name := stringChange(line)
-		domainName := strings.Split(name, ".")
-		if domainName[1] == domain {
-			if _, exists := stat[name]; exists {
-				stat[name]++
-			} else {
-				stat[name] = 1
+
+	for {
+		line, err := u.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				panic(err)
 			}
+			if len(line) > 0 {
+				stat = countChanges(stat, domain, line)
+			}
+			break
 		}
+
+		stat = countChanges(stat, domain, line)
+
 	}
 	return stat, nil
 }
 
+func countChanges(stat DomainStat, domain string, line string) DomainStat {
+	name := stringChange(line)
+	domainName := strings.Split(name, ".")
+	if domainName[1] == domain {
+		if _, exists := stat[name]; exists {
+			stat[name]++
+		} else {
+			stat[name] = 1
+		}
+	}
+	return stat
+}
+
 func stringChange(line string) string {
 	line = strings.ToLower(line)
-	user := strings.Split(line, ",")
-	email := strings.Split(user[3], ":")
-	newEmail := strings.ReplaceAll(email[1], "\"", "")
-	emailName := strings.Split(newEmail, "@")
-	return emailName[1]
+	//user := strings.Split(line, ",")
+	//email := strings.Split(strings.Split(line, ",")[3], ":")
+	//newEmail := strings.ReplaceAll(strings.Split(strings.Split(line, ",")[3], ":")[1], "\"", "")
+	//emailName := strings.Split(strings.ReplaceAll(strings.Split(strings.Split(line, ",")[3], ":")[1], "\"", ""), "@")
+	return strings.Split(strings.ReplaceAll(strings.Split(strings.Split(line, ",")[3], ":")[1], "\"", ""), "@")[1]
 }
